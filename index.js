bd416fdfc41982e3cc2abeae2f8257b582d2ed0c
yjglab <yjgdesign@gmail.com>
2020-12-31 22:54:25 +0900

    update screen

--- index.js
+++ index.js
@@ -34,30 +34,76 @@ function deckToField(data, whoseTurn) {
   let index = obj.deckData.indexOf(data);
   obj.deckData.splice(index, 1);
   obj.fieldData.push(data);
-  obj.deck.innerHTML = "";
-  obj.field.innerHTML = "";
-  obj.fieldData.forEach((data) => {
-    cardDomeConcat(data, obj.field);
-  });
-  obj.deckData.forEach((data) => {
-    cardDomeConcat(data, obj.deck);
-  });
+  reprintField(obj);
+  reprintDeck(obj);
   data.field = true;
   obj.cost.textContent = currentCost - data.cost;
 }
 
-function reprintScreen(whoseScreen) {
-  let obj = whoseScreen ? p1 : p2;
-  obj.deck.innerHTML = "";
+function reprintField(obj) {
   obj.field.innerHTML = "";
-  obj.hero.innerHTML = "";
   obj.fieldData.forEach((data) => {
     cardDomeConcat(data, obj.field);
   });
+}
+function reprintDeck(obj) {
+  obj.deck.innerHTML = "";
   obj.deckData.forEach((data) => {
     cardDomeConcat(data, obj.deck);
   });
+}
+function reprintHero(obj) {
+  obj.hero.innerHTML = "";
   cardDomeConcat(obj.heroData, obj.hero, true);
+}
+function reprintScreen(whoseScreen) {
+  let obj = whoseScreen ? p1 : p2;
+  reprintField(obj);
+  reprintDeck(obj);
+  reprintHero(obj);
+}
+
+function battle(card, data, whoseTurn) {
+  let attacker = whoseTurn ? p1 : p2;
+  let defender = whoseTurn ? p2 : p1;
+  if (card.classList.contains("card-turnover")) {
+    return;
+  }
+  let defenderCard = whoseTurn ? !data.mine : data.mine;
+  if (defenderCard && attacker.selectedCard) {
+    data.hp = data.hp - attacker.selectedCardData.att;
+    if (data.hp <= 0) {
+      let index = defender.fieldData.indexOf(data);
+      if (index > -1) {
+        // 졸 사망 시
+        defender.fieldData.splice(index, 1);
+      } else {
+        // 영웅 사망 시
+        // 승리메시지
+        init(); // 셋타임아웃
+      }
+    }
+    reprintScreen(!whoseTurn);
+    attacker.selectedCard.classList.remove("card-selected"); // 공격 종료
+    attacker.selectedCard.classList.add("card-turnover");
+    attacker.selectedCard = null;
+    attacker.selectedCardData = null;
+    return;
+  } else if (defenderCard) {
+    return;
+  }
+  if (data.field) {
+    card.parentNode.parentNode.querySelectorAll(".card").forEach((card) => {
+      card.classList.remove("card-selected");
+    });
+    card.classList.add("card-selected");
+    attacker.selectedCard = card;
+    attacker.selectedCardData = data;
+  } else {
+    if (deckToField(data, whoseTurn) !== "end") {
+      whoseTurn ? createP1Deck(1) : createP2Deck(1);
+    }
+  }
 }
 function cardDomeConcat(data, dome, hero) {
   let card = document.querySelector(".card-hidden .card").cloneNode(true);
@@ -72,114 +118,21 @@ function cardDomeConcat(data, dome, hero) {
     card.appendChild(heroName);
   }
   card.addEventListener("click", () => {
-    if (turn) {
-      // P1의 턴
-      if (card.classList.contains("card-turnover")) {
-        return;
-      }
-      if (!data.whoseCard && p1.selectedCard) {
-        if (p1.cost.textContent >= 1) {
-          // 여기에 텍스트..
-          return; // 코스트를 다 쓰지 않으면 공격 불가
-        }
-        data.hp = data.hp - p1.selectedCardData.att;
-        if (data.hp <= 0) {
-          let index = p2.fieldData.indexOf(data);
-          if (index > -1) {
-            // 졸 사망 시
-            p2.fieldData.splice(index, 1);
-          } else {
-            // 영웅 사망 시
-            // 승리메시지
-            init(); // 셋타임아웃
-          }
-        }
-        reprintScreen(false);
-        p1.selectedCard.classList.remove("card-selected"); // 공격 종료
-        p1.selectedCard.classList.add("card-turnover");
-        p1.selectedCard = null;
-        p1.selectedCardData = null;
-        return;
-      } else if (!data.whoseCard) {
-        return;
-      }
-      if (data.field) {
-        card.parentNode.parentNode.querySelectorAll(".card").forEach((card) => {
-          card.classList.remove("card-selected");
-        });
-        card.classList.add("card-selected");
-        p1.selectedCard = card;
-        p1.selectedCardData = data;
-      } else {
-        if (deckToField(data, true) !== "end") {
-          createP1Deck(1);
-        }
-      }
-    } else {
-      // P2의 턴
-      if (card.classList.contains("card-turnover")) {
-        return;
-      }
-      if (data.whoseCard && p2.selectedCard) {
-        if (p2.cost.textContent >= 1) {
-          // 여기에 텍스트..
-          return; // 코스트를 다 쓰지 않으면 공격 불가
-        }
-        data.hp = data.hp - p2.selectedCardData.att;
-        if (data.hp <= 0) {
-          let index = p1.fieldData.indexOf(data);
-          if (index > -1) {
-            // 졸 사망 시
-            p1.fieldData.splice(index, 1);
-          } else {
-            // 영웅 사망 시
-            // 승리메시지
-            init(); // 셋타임아웃
-          }
-        }
-        reprintScreen(true);
-        p2.selectedCard.classList.remove("card-selected"); // 공격 종료
-        p2.selectedCard.classList.add("card-turnover");
-        p2.selectedCard = null;
-        p2.selectedCardData = null;
-        return;
-      } else if (data.whoseCard) {
-        return;
-      }
-      if (data.field) {
-        card.parentNode.parentNode.querySelectorAll(".card").forEach((card) => {
-          card.classList.remove("card-selected");
-        });
-        card.classList.add("card-selected");
-        p2.selectedCard = card;
-        p2.selectedCardData = data;
-      } else {
-        if (deckToField(data, false) !== "end") {
-          createP2Deck(1);
-        }
-      }
-    }
+    battle(card, data, turn);
   });
-
   dome.appendChild(card); // 시각적 삽입
 }
 function createP2Deck(num) {
   for (let i = 0; i < num; i++) {
     p2.deckData.push(cardFactory(false, false));
   }
-  p2.deck.innerHTML = "";
-  p2.deckData.forEach((data) => {
-    cardDomeConcat(data, p2.deck);
-  });
+  reprintDeck(p2);
 }
 function createP1Deck(num) {
   for (let i = 0; i < num; i++) {
     p1.deckData.push(cardFactory(false, true)); // hero-false, whoseCard-true
   }
-  p1.deck.innerHTML = "";
-  p1.deckData.forEach((data) => {
-    cardDomeConcat(data, p1.deck);
-  });
+  reprintDeck(p1);
 }
 function createP2Hero() {
   p2.heroData = cardFactory(true, false);
@@ -226,12 +179,8 @@ turnBtn.addEventListener("click", () => {
   let obj = turn ? p1 : p2;
   document.querySelector(".p2").classList.toggle("turn");
   document.querySelector(".p1").classList.toggle("turn");
-  obj.field.innerHTML = "";
-  obj.hero.innerHTML = "";
-  obj.fieldData.forEach((data) => {
-    cardDomeConcat(data, obj.field);
-  });
-  cardDomeConcat(obj.heroData, obj.hero, true);
+  reprintField(obj);
+  reprintHero(obj);
   turn = !turn;
   if (turn) {
     p1.cost.textContent = 10;
